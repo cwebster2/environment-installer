4f02d582811e4acde4966eaeda7ccf3cc1368b1e
Casey Webster <casey.webster@gmail.com>
2020-03-24 16:28:54 -0500

    Formatting update; add expect

--- install-stage0-ubuntu.sh
+++ install-stage0-ubuntu.sh
@@ -8,24 +8,24 @@ export TARGET_USER=${TARGET_USER:-casey}
 
 # Choose a user account to use for this installation
 get_user() {
-	if [ -z "${TARGET_USER-}" ]; then
-		mapfile -t options < <(find /home/* -maxdepth 0 -printf "%f\\n" -type d)
-		# if there is only one option just use that user
-		if [ "${#options[@]}" -eq "1" ]; then
-			readonly TARGET_USER="${options[0]}"
-			echo "Using user account: ${TARGET_USER}"
-			return
-		fi
+  if [ -z "${TARGET_USER-}" ]; then
+    mapfile -t options < <(find /home/* -maxdepth 0 -printf "%f\\n" -type d)
+    # if there is only one option just use that user
+    if [ "${#options[@]}" -eq "1" ]; then
+      readonly TARGET_USER="${options[0]}"
+      echo "Using user account: ${TARGET_USER}"
+      return
+    fi
 
-		# iterate through the user options and print them
-		PS3='command -v user account should be used? '
+    # iterate through the user options and print them
+    PS3='command -v user account should be used? '
 
-		select opt in "${options[@]}"; do
-			readonly TARGET_USER=$opt
-			break
-		done
-		fi
-	}
+    select opt in "${options[@]}"; do
+      readonly TARGET_USER=$opt
+      break
+    done
+  fi
+}
 
 check_is_sudo() {
   if [ "$EUID" -ne 0 ]; then
@@ -52,11 +52,11 @@ setup_sources_min() {
 }
 
  # sets up apt sources
-setup_sources() {
-  setup_sources_min;
+ setup_sources() {
+   setup_sources_min;
 
-  add-apt-repository -y ppa:yubico/stable
-  add-apt-repository -y ppa:wireguard/wireguard
+   add-apt-repository -y ppa:yubico/stable
+   add-apt-repository -y ppa:wireguard/wireguard
 
 
   # tlp: Advanced Linux Power Management
@@ -83,27 +83,27 @@ EOF
   deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 EOF
 
-  cat <<- EOF > /etc/apt/sources.list.d/keybase.list
+cat <<- EOF > /etc/apt/sources.list.d/keybase.list
   deb http://prerelease.keybase.io/deb stable main
 EOF
 
-  cat <<- EOF > /etc/apt/sources.list.d/microsoft-prod.list
+cat <<- EOF > /etc/apt/sources.list.d/microsoft-prod.list
   deb [arch=amd64] https://packages.microsoft.com/ubuntu/19.10/prod eoan main
 EOF
 
-  cat <<- EOF > /etc/apt/sources.list.d/teams.list
+cat <<- EOF > /etc/apt/sources.list.d/teams.list
    deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main
 EOF
 
-  cat <<- EOF > /etc/apt/sources.list.d/vscode.list
+cat <<- EOF > /etc/apt/sources.list.d/vscode.list
    deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main
 EOF
 
-  cat <<- EOF > /etc/apt/sources.list.d/slack.list
+cat <<- EOF > /etc/apt/sources.list.d/slack.list
   deb https://packagecloud.io/slacktechnologies/slack/debian/ jessie main
 EOF
 
-  cat <<- EOF > /etc/apt/sources.list.d/spotify.list
+cat <<- EOF > /etc/apt/sources.list.d/spotify.list
   deb http://repository.spotify.com stable non-free
 EOF
 
@@ -134,90 +134,91 @@ EOF
 }
 
 base_min() {
-	apt-get update || true
-	apt-get -y upgrade
+  apt-get update || true
+  apt-get -y upgrade
 
-	apt-get install -y \
-		adduser \
-		automake \
-		bc \
-		bzip2 \
-		ca-certificates \
-		coreutils \
-		curl \
-		dnsutils \
+  apt-get install -y \
+    adduser \
+    automake \
+    bc \
+    bzip2 \
+    ca-certificates \
+    coreutils \
+    curl \
+    dnsutils \
+    expect \
     fd-find \
-		file \
-		findutils \
-		gcc \
-		git \
-		gnupg \
-		gnupg2 \
-		grep \
-		gzip \
-		hostname \
-		indent \
-		iptables \
+    file \
+    findutils \
+    gcc \
+    git \
+    gnupg \
+    gnupg2 \
+    grep \
+    gzip \
+    hostname \
+    indent \
+    iptables \
     iw \
-		jq \
-		less \
-		locales \
-		lsof \
-		make \
-		mount \
+    jq \
+    less \
+    locales \
+    lsof \
+    make \
+    mount \
     net-tools \
-		policykit-1 \
-		silversearcher-ag \
-		ssh \
-		strace \
-		sudo \
-		tar \
-		tree \
-		tzdata \
-		unzip \
+    policykit-1 \
+    silversearcher-ag \
+    ssh \
+    strace \
+    sudo \
+    tar \
+    tree \
+    tzdata \
+    unzip \
     neovim \
     emacs-gtk \
     wget \
-		xz-utils \
-		zip \
+    xz-utils \
+    zip \
     plymouth-label \
     plymouth-themes \
-		--no-install-recommends
+    --no-install-recommends
 
-	apt -y autoremove
-	apt autoclean
-	apt clean
+  apt -y autoremove
+  apt autoclean
+  apt clean
 }
 
 # installs base packages
 # the utter bare minimal shit
 base() {
-	base_min;
+  base_min;
 
-	apt-get update || true
-	apt-get -y upgrade
+  apt-get update || true
+  apt-get -y upgrade
 
-	apt-get install -y \
-		apparmor \
+  apt-get install -y \
+    apparmor \
     bluez \
     bolt \
-		bridge-utils \
+    bridge-utils \
     build-essential \
-		cgroupfs-mount \
+    cgroupfs-mount \
     cpufrequtils \
-		fwupd \
-		fwupdate \
-		gnupg-agent \
-		google-cloud-sdk \
-		iwd \
+    fwupd \
+    fwupdate \
+    gnupg-agent \
+    google-cloud-sdk \
+    iwd \
     lastpass-cli \
-		libimobiledevice6 \
-		libpam-systemd \
+    libimobiledevice6 \
+    libpam-systemd \
     pcscd \
-		pinentry-curses \
-		scdaemon \
+    pinentry-curses \
+    scdaemon \
     psmisc \
-		systemd \
+    systemd \
     fzf \
     gdm3 \
     htop \
@@ -244,19 +245,19 @@ base() {
     bat \
     libsecret-1-dev \
     libssl-dev \
-		--no-install-recommends
+    --no-install-recommends
 
-	setup_sudo
+  setup_sudo
 
-	apt -y autoremove
-	apt autoclean
-	apt clean
+  apt -y autoremove
+  apt autoclean
+  apt clean
 
   sed -i '/en_US.UTF-8/ s/^# //' /etc/locale.gen
   locale-gen
   dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
-}
+  }
 
 # setup sudo for a user
 # because fuck typing that shit all the time
@@ -266,84 +267,88 @@ base() {
 # so its pointless
 # i know what the fuck im doing ;)
 setup_sudo() {
-	# add user to sudoers
-	adduser "$TARGET_USER" sudo
+  # add user to sudoers
+  adduser "$TARGET_USER" sudo
 
-	# add user to systemd groups
-	# then you wont need sudo to view logs and shit
-	gpasswd -a "$TARGET_USER" systemd-journal
-	gpasswd -a "$TARGET_USER" systemd-network
+  # add user to systemd groups
+  # then you wont need sudo to view logs and shit
+  gpasswd -a "$TARGET_USER" systemd-journal
+  gpasswd -a "$TARGET_USER" systemd-network
 
-	# create docker group
-	sudo groupadd -f docker
-	sudo gpasswd -a "$TARGET_USER" docker
+  # create docker group
+  sudo groupadd -f docker
+  sudo gpasswd -a "$TARGET_USER" docker
 
-	# add go path to secure path
-	{ \
-		echo -e "Defaults	secure_path=\"/usr/local/go/bin:/home/${TARGET_USER}/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/share/bcc/tools:/home/${TARGET_USER}/.cargo/bin\""; \
-		echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
-		echo -e "${TARGET_USER} ALL=(ALL) NOPASSWD:ALL"; \
-		echo -e "${TARGET_USER} ALL=NOPASSWD: /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
-	} >> /etc/sudoers
+  # add go path to secure path
+  { \
+    echo -e "Defaults	secure_path=\"/usr/local/go/bin:/home/${TARGET_USER}/.go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/share/bcc/tools:/home/${TARGET_USER}/.cargo/bin\""; \
+    echo -e 'Defaults	env_keep += "ftp_proxy http_proxy https_proxy no_proxy GOPATH EDITOR"'; \
+    echo -e "${TARGET_USER} ALL=(ALL) NOPASSWD:ALL"; \
+    echo -e "${TARGET_USER} ALL=NOPASSWD: /sbin/ifconfig, /sbin/ifup, /sbin/ifdown, /sbin/ifquery"; \
+  } >> /etc/sudoers
 
-	# setup downloads folder as tmpfs
-	# that way things are removed on reboot
-	# i like things clean but you may not want this
-	mkdir -p "/home/${TARGET_USER}/Downloads"
-	echo -e "\\n# tmpfs for downloads\\ntmpfs\\t/home/${TARGET_USER}/Downloads\\ttmpfs\\tnodev,nosuid,size=2G\\t0\\t0" >> /etc/fstab
-  sudo mount "/home/${TARGET_USER}/Downloads"
-  chown ${TARGET_USER}:${TARGET_USER} "/home/${TARGET_USER}/Downloads"
+  # setup downloads folder as tmpfs
+  # that way things are removed on reboot
+  # i like things clean but you may not want this
+  (
+    set +e
+    mkdir -p "/home/${TARGET_USER}/Downloads"
+    echo -e "\\n# tmpfs for downloads\\ntmpfs\\t/home/${TARGET_USER}/Downloads\\ttmpfs\\tnodev,nosuid,size=2G\\t0\\t0" >> /etc/fstab
+    sudo mount "/home/${TARGET_USER}/Downloads"
+    chown ${TARGET_USER}:${TARGET_USER} "/home/${TARGET_USER}/Downloads"
+    echo "Mounted user Download directory as tmpfs"
+  )
 }
 
 # install graphics drivers
 install_graphics() {
-	local system=$1
+  local system=$1
 
-	if [[ -z "$system" ]]; then
-		echo "You need to specify whether it's intel, geforce or optimus"
-		exit 1
-	fi
+  if [[ -z "$system" ]]; then
+    echo "You need to specify whether it's intel, geforce or optimus"
+    exit 1
+  fi
 
-	local pkgs=( xorg xserver-xorg xserver-xorg-input-libinput xserver-xorg-input-synaptics )
+  local pkgs=( xorg xserver-xorg xserver-xorg-input-libinput xserver-xorg-input-synaptics )
 
-	case $system in
-		"intel")
-			pkgs+=( xserver-xorg-video-intel )
-			;;
-		"geforce")
-			pkgs+=( nvidia-driver-440 )
-			;;
-		"optimus")
-			pkgs+=( nvidia-kernel-dkms bumblebee-nvidia primus )
-			;;
-		*)
-			echo "You need to specify whether it's intel, geforce or optimus"
-			exit 1
-			;;
-	esac
+  case $system in
+    "intel")
+      pkgs+=( xserver-xorg-video-intel )
+      ;;
+    "geforce")
+      pkgs+=( nvidia-driver-440 )
+      ;;
+    "optimus")
+      pkgs+=( nvidia-kernel-dkms bumblebee-nvidia primus )
+      ;;
+    *)
+      echo "You need to specify whether it's intel, geforce or optimus"
+      exit 1
+      ;;
+  esac
 
-	apt update || true
-	apt -y upgrade
+  apt update || true
+  apt -y upgrade
 
-	apt install -y "${pkgs[@]}" --no-install-recommends
+  apt install -y "${pkgs[@]}" --no-install-recommends
 }
 
 # install stuff for i3 window manager
 install_wmapps() {
-	apt update || true
-	apt install -y \
-		alsa-utils \
-		feh \
-		i3 \
-		i3lock-fancy \
-		i3status \
-		flameshot \
-		suckless-tools \
-		kitty \
+  apt update || true
+  apt install -y \
+    alsa-utils \
+    feh \
+    i3 \
+    i3lock-fancy \
+    i3status \
+    flameshot \
+    suckless-tools \
+    kitty \
     rofi \
-		usbmuxd \
-		xclip \
-		compton \
+    usbmuxd \
+    xclip \
+    compton \
     arandr \
     adwaita-icon-theme \
     breeze-cursor-theme \
@@ -374,57 +379,57 @@ install_wmapps() {
     code-insiders \
     spotify-client \
     keybase \
-		--no-install-recommends
+    --no-install-recommends
 
-	  apt -y autoremove
-	  apt autoclean
-	  apt clean
+  apt -y autoremove
+  apt autoclean
+  apt clean
 }
 
 usage() {
-	echo -e "install.sh\\n\\tThis script installs my basic setup for a debian laptop\\n"
-	echo "Usage:"
-	echo "  base                                - setup sources & install base pkgs"
-	echo "  basemin                             - setup sources & install base min pkgs"
-	echo "  graphics {intel, geforce, optimus}  - install graphics drivers"
-	echo "  wm                                  - install window manager/desktop pkgs"
+  echo -e "install.sh\\n\\tThis script installs my basic setup for a debian laptop\\n"
+  echo "Usage:"
+  echo "  base                                - setup sources & install base pkgs"
+  echo "  basemin                             - setup sources & install base min pkgs"
+  echo "  graphics {intel, geforce, optimus}  - install graphics drivers"
+  echo "  wm                                  - install window manager/desktop pkgs"
 }
 
 main() {
-	local cmd=$1
+  local cmd=$1
 
-	if [[ -z "$cmd" ]]; then
-		usage
-		exit 1
-	fi
+  if [[ -z "$cmd" ]]; then
+    usage
+    exit 1
+  fi
 
-	if [[ $cmd == "base" ]]; then
-		check_is_sudo
-		get_user
+  if [[ $cmd == "base" ]]; then
+    check_is_sudo
+    get_user
 
-		# setup /etc/apt/sources.list
-		setup_sources
+    # setup /etc/apt/sources.list
+    setup_sources
 
-		base
-	elif [[ $cmd == "basemin" ]]; then
-		check_is_sudo
-		get_user
+    base
+  elif [[ $cmd == "basemin" ]]; then
+    check_is_sudo
+    get_user
 
-		# setup /etc/apt/sources.list
-		setup_sources_min
+    # setup /etc/apt/sources.list
+    setup_sources_min
 
-		base_min
-	elif [[ $cmd == "graphics" ]]; then
-		check_is_sudo
+    base_min
+  elif [[ $cmd == "graphics" ]]; then
+    check_is_sudo
 
-		install_graphics "$2"
-	elif [[ $cmd == "wm" ]]; then
-		check_is_sudo
+    install_graphics "$2"
+  elif [[ $cmd == "wm" ]]; then
+    check_is_sudo
 
-		install_wmapps
-	else
-		usage
-	fi
+    install_wmapps
+  else
+    usage
+  fi
 }
 
 main "$@"
