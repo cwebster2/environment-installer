bb17240512c879523e369f7987384d20e234b9bf
Casey Webster <casey.webster@gmail.com>
2020-04-17 11:07:35 -0500

    Iteration 2

---
+++ prepare-ubuntu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+export DISK=${DISK:-"/dev/disk/by-id/scsi-SATA_disk1"}
+
+wget -qO- https://raw.githubusercontent.com/cwebster2/environment-installer/master/install-stage0pre-ubunutu.sh > install-stage0pre.sh
+
+chmod 755 install-stage0pre.sh
+
+echo "Running prepare requires sudo, you will be promped for your password"
+
+echo "This process is destructive to any data on ${DISK}.  BEWARE"
+
+sudo ./install-stagepre0.sh
