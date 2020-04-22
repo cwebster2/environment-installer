42a1cda81d6bb62a78397bba439cf10ba21b008c
Casey Webster <casey.webster@gmail.com>
2020-04-21 21:25:50 -0500

    quoting

--- install-stage0pre-ubuntu.sh
+++ install-stage0pre-ubuntu.sh
@@ -214,7 +214,7 @@ EOF
     adduser --home /home/${TARGET_USER} --shell /usr/bin/bash --uid 1000 ${TARGET_USER}
     usermod -a -G adm,cdrom,dip,lpadmin,plugdev,sambashare,sudo ${TARGET_USER}
     cat > /home/${TARGET_USER}/do-stage0-install.sh <<-EOF
-DOTFILESBRANCH=razer-ubuntu INSTALLER=ubuntu bash -c "$(wget -qO- https://raw.githubusercontent.com/cwebster2/environment-installer/master/install.sh)"  | tee install.log
+DOTFILESBRANCH=razer-ubuntu INSTALLER=ubuntu bash -c '"$(wget -qO- https://raw.githubusercontent.com/cwebster2/environment-installer/master/install.sh)"'  | tee install.log
 EOF
     chmod 755 /home/${TARGET_USER}/do-stage0-install.sh
     chown -R ${TARGET_USER}.${TARGET_USER} /home/${TARGET_USER}
