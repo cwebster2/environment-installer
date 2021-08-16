8c578cb0ea183367d40a6faf924fa010516a9dd2
Casey Webster <casey.webster@gmail.com>
2021-08-16 15:22:17 -0500

    Add prepare script, update install

--- install.sh
+++ install.sh
@@ -9,20 +9,19 @@ set -o pipefail
 
 export TARGET_USER=$(whoami)
 export DOTFILESBRANCH=${DOTFILESBRANCH:-master}
-export INSTALLER=${INSTALLER:-debian}
-export GRAPHICS=${GRAPHICS:-geforce}
+export INSTALLER=${INSTALLER:-arch}
+export GRAPHICS=${GRAPHICS:-intel}
 
-wget -qO- https://raw.githubusercontent.com/cwebster2/environment-installer/master/install-stage0-${INSTALLER}.sh > install-stage0.sh
-wget -qO- https://raw.githubusercontent.com/cwebster2/dotfiles/${DOTFILESBRANCH}/bin/install.sh > install-stage1.sh
+curl -o install-stage0.sh https://raw.githubusercontent.com/cwebster2/environment-installer/master/install-stage0-${INSTALLER}.sh
+curl -o install-stage1.sh https://raw.githubusercontent.com/cwebster2/dotfiles/${DOTFILESBRANCH}/bin/install.sh
 
 chmod 755 install-stage0.sh install-stage1.sh
 
 echo "Running stage0 requires sudo, you will be prompted for your password"
 
-sudo ./install-stage0.sh basemin
 sudo ./install-stage0.sh base
-sudo ./install-stage0.sh graphics ${GRAPHICS}
 sudo ./install-stage0.sh wm
+sudo ./install-stage0.sh games
 
 echo "Installing dotfiles"
 
@@ -31,6 +30,8 @@ echo "Installing dotfiles"
 echo "Initializing user environment"
 
 zsh -i -c "./install-stage1.sh tools"
+zsh -i -c "./install-stage1.sh vim"
+zsh -i -c "./install-stage1.sh emacs"
 
 echo "Cleaning up"
 
---
+++ prepare.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install with the following command
+# bash -c "$(wget -qO- https://raw.githubusercontent.com/cwebster2/environment-installer/master/prepare.sh)"
+# This assumes you already have a working OS and user created
+
+set -e
+set -u
+set -o pipefail
+
+export TARGET_USER=
+export INSTALLER=${INSTALLER:-arch}
+export SWAPSIZE=${SWAPSIZE:-32}
+export SSID=""
+export WPA_PASSPHRASE=""
+export HOSTNAME=""
+
+curl -o install-stage0.sh https://raw.githubusercontent.com/cwebster2/environment-installer/master/install-stage0-${INSTALLER}.sh
+
+chmod 755 install-stage0.sh
+
+echo "Running stage0 prepare.  You will be prompted for zfs passphrase and user password"
+
+./install-stage0.sh prepare
+
+rm install-stage0.sh
+
+reboot
